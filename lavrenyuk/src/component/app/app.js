a88d11c650750559d16cbd58b16f357e166db309
Илья Лавренюк <48129985+iLavrenyuk@users.noreply.github.com>
2019-06-14 02:29:30 +0300

    Add files via upload

---
+++ lavrenyuk/src/component/app/app.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+import './app.css';
+
+const App = () => {
+
+    const data = [
+        [1],
+        { label: "Going ot learn Rect", important: true, id: "daswe" },
+        { label: "That is so good", important: false, id: "edsfw" },
+        { label: "I need a break...", important: false, id: "dsfdf" }
+    ]
+
+    return (
+        <div className="app">
+            <AppHeader />
+            <div className="search-panel d-flex">
+                <SearchPanel />
+                <PostStatusFilter />
+            </div>
+            <PostList posts={data} />
+            <PostAddForm />
+        </div>
+    )
+}
+
+export default App;
---
+++ lavrenyuk/src/component/post-list/post-list.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './post-list.css';
+
+import PostListItem from '../post-list-item'
+
+const PostList = ({ posts }) => {
+
+    const elements = posts.map((item) => {
+        const { id, ...itemProps } = item;
+        if (item.constructor === Object) {
+
+            return (
+                <li key={id} className='list-group-item'>
+                    <PostListItem {...itemProps} />
+                </li>
+            )
+        }
+    });
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default PostList;
